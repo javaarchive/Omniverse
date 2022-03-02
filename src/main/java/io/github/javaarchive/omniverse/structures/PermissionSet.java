667363c877cd0d5ad8e21c09023431d7f5430c1d
Raymond <raymond.peng07@gmail.com>
2022-03-02 17:56:46 +0000

    hydrate method

--- src/main/java/io/github/javaarchive/omniverse/structures/PermissionSet.java
+++ src/main/java/io/github/javaarchive/omniverse/structures/PermissionSet.java
@@ -3,15 +3,20 @@ package io.github.javaarchive.omniverse.structures;
 import java.util.List;
 import java.util.Map;
 
+import lombok.Getter;
+
 public class PermissionSet {
-    //  Role        Permission
-    Map<String, Map<String,  Permission>> pData;
+    //         Role        Permission    Value
+    public Map<String, Map<String    ,  Permission>> pData;
 
-    List<String> roleList;
+    @Getter private List<String> roleList;
 
-    Map<String, Integer> role;
+    private transient Map<String, Integer> roleInverseIndex; // Maps role names to position
 
     public PermissionSet hydrate(){
+        for(int i = 0; i < roleList.size(); i ++){
+            this.roleInverseIndex.put(roleList.get(i), i);
+        }
         return this;
     }
 
