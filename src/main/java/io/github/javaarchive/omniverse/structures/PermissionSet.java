2c74a62d7e48bae923a7d8779394f487af1ad15a
Raymond <raymond.peng07@gmail.com>
2022-03-03 17:39:12 +0000

    role utils

--- src/main/java/io/github/javaarchive/omniverse/structures/PermissionSet.java
+++ src/main/java/io/github/javaarchive/omniverse/structures/PermissionSet.java
@@ -1,5 +1,6 @@
 package io.github.javaarchive.omniverse.structures;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
@@ -13,10 +14,30 @@ public class PermissionSet {
 
     private transient Map<String, Integer> roleInverseIndex; // Maps role names to position
 
+    public Permission setPerm(String role, String perm, Permission value){
+        Permission oldValue = pData.get(role).get(perm);
+        pData.get(role).put(perm, value);
+        return oldValue;
+    }
+
+    public Permission getPerm(String role, String perm){
+        return pData.get(role).get(perm);
+    }
+
+    private List<String> sortRoles(List<String> roles){
+        Map<String, Integer> roleInv = roleInverseIndex;
+        roles.sort(new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return roleInv.get(o2) - roleInv.get(o1);
+            }
+        });
+        return roles;
+    }
+
     public PermissionSet hydrate(){
-        for(int i = 0; i < roleList.size(); i ++){
-            this.roleInverseIndex.put(roleList.get(i), i);
-        }
+        this.rebuildInvIndex();
         return this;
     }
 
@@ -32,4 +53,6 @@ public class PermissionSet {
             this.roleInverseIndex.put(roleList.get(i), i);
         }
     }
+
+
 }
